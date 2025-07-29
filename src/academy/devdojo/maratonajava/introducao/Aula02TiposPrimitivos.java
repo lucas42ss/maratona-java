bee7e0793da413fc7ad712f368e84998e27a53ef
lucas42ss <emanoelucasmsz@gmail.com>
2025-07-28 21:33:32 -0300

    docs: inicio aula 2 tipos primitivos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void  main(String[] arg){
+        //tipos primitivos -> int, double, float, char, byte, short, long, boolean
+        int age = 25;
+        for(int i=0; i<50; i++){
+            System.out.println(i);
+        }
+    }
+}
