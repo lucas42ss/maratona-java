e3f3c3464a7b388c431d00bd7aeea832191d5f09
lucas42ss <emanoelucasmsz@gmail.com>
2025-08-06 21:33:05 -0300

    docs: revisão

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/teste/LeituraDoTecladoTeste01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.teste;
+
+import java.util.Scanner;
+
+public class LeituraDoTecladoTeste01 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String next = entrada.nextLine();
+
+        System.out.println(next);
+
+    }
+}
