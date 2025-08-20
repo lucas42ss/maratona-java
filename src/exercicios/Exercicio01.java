bb4bbb9ea841c10b5bf2224f088919d7e0aa76bd
lucas42ss <emanoelucasmsz@gmail.com>
2025-08-20 17:18:58 -0300

    docs: inicio exercicios

---
+++ src/exercicios/Exercicio01.java
@@ -0,0 +1,27 @@
+package exercicios;
+
+//1 - Faça um algoritmo que leia os valores de A, B, C e em seguida
+// imprima na tela a soma entre A e B é mostre se a soma é menor que C.
+
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Scanner next = new Scanner(System.in);
+        System.out.println("Insira 3 números.");
+        int a = next.nextInt();
+        int b = next.nextInt();
+        int c = next.nextInt();
+
+        System.out.println(a + b);
+
+        if ((a + b) < c) {
+            System.out.println("Soma de A e B é menor que C");
+        }else if((a+b)==c){
+            System.out.println("Soma de A e B é igual que C");
+        }else{
+            System.out.println("Soma de A e B é maior que C");
+        }
+    }
+}
